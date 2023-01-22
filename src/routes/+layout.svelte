ba0213d8bc265e08cc426a48b77e7c5e518ec64d
Suyashtnt <suyashtnt@gmail.com>
2023-01-22 22:40:14 +0200

    refactor: optimize fonts by only including regular sizing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import './app.css'
-    import '@fontsource/inter'
+    import '@fontsource/inter/400.css'
 </script>
 
 <main class="flex flex-col h-full bg-light-base text-light-text dark:bg-dark-base dark:text-dark-text">
