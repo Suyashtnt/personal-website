7850339fa01d9dd2b798fc86acc8754275414449
Suyashtnt <suyashtnt@gmail.com>
2023-01-22 22:24:08 +0200

    feat: add some animations, adjust text sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,137 @@
 <script lang="ts">
-    import 'atropos/css'
-    import Atropos from 'atropos/svelte'
+	import 'atropos/css';
+	import Atropos from 'atropos/svelte';
+	import anime from 'animejs';
+	import { browser } from '$app/environment';
+
+	function animateWord(word: 'student' | 'programmer' | 'gamer') {
+		anime.remove(`.${word} .letter`);
+
+		anime({
+			targets: `.${word} .letter`,
+			autoplay: true,
+			rotate: () => anime.random(-30, 30),
+			translateX: () => anime.random(30, 30),
+			translateY: () => anime.random(-30, 30)
+		});
+	}
+
+	function endAnimateWord(word: 'student' | 'programmer' | 'gamer') {
+		anime.remove(`.${word} .letter`);
+
+		anime({
+			targets: `.${word} .letter`,
+			autoplay: true,
+			rotate: 0,
+			translateX: 0,
+			translateY: 0
+		});
+	}
+
+	if (browser) {
+		const opacityIn = [0, 1];
+		const transYIn = [-50, 0];
+		const transYOut = [0, 50];
+		const durationIn = 800;
+		const durationOut = 600;
+		const delay = 1000;
+
+		anime
+			.timeline({ loop: true, autoplay: true })
+			.add({
+				targets: `.names .name-1`,
+				opacity: opacityIn,
+				translateY: transYIn,
+				duration: durationIn
+			})
+			.add({
+				targets: `.names .name-1`,
+				opacity: 0,
+				translateY: transYOut,
+				duration: durationOut,
+				easing: 'easeInExpo',
+				delay: delay
+			})
+			.add({
+				targets: `.names .name-2`,
+				opacity: opacityIn,
+				translateY: transYIn,
+				duration: durationIn
+			})
+			.add({
+				targets: `.names .name-2`,
+				opacity: 0,
+				translateY: transYOut,
+				duration: durationOut,
+				easing: 'easeInExpo',
+				delay: delay
+			})
+			.add({
+				targets: `.names .name-3`,
+				opacity: opacityIn,
+				translateY: transYIn,
+				duration: durationIn
+			})
+			.add({
+				targets: `.names .name-3`,
+				opacity: 0,
+				translateY: transYOut,
+				duration: durationOut,
+				easing: 'easeInExpo',
+				delay: delay
+			});
+	}
 </script>
 
 <section class="flex flex-row flex-wrap justify-around content-center">
-    <Atropos class="rounded-xl aspect-square pa-8">
-        <!-- placeholder image here -->
-        <div class="flex content-center h-full w-full justify-center">
-            <img src="https://picsum.photos/400/500" alt="placeholder" class="rounded-xl" />
-        </div>
-    </Atropos>
-    <div class="flex-grow">
-        <h1 class="text-9xl w-max leading-30">
-            Hi,
-            <br>
-            I'm TNTMan1671
-            <h2 class="text-6xl mt-0 flex justify-between leading-0 desc">
-                <p class="word">Student</p> <p class="word">Programmer</p> <p class="word">Gamer</p>
-            </h2>
-        </h1>
-    </div>
+	<Atropos class="rounded-xl aspect-square px-8 pt-8">
+		<!-- placeholder image here -->
+		<div class="flex content-center h-full w-full justify-center">
+			<img src="https://picsum.photos/400/500" alt="placeholder" class="rounded-xl" />
+		</div>
+	</Atropos>
+	<div class="flex-grow">
+		<h1 class="text-8xl leading-30">
+			Hi,
+			<br />
+			I'm
+			<span class="names ml-4">
+				<span class="name-1 inline-block opacity-0 absolute">TNTMan1671</span>
+				<span class="name-2 inline-block opacity-0 absolute">A badly drawn wobbler</span>
+				<span class="name-3 inline-block opacity-0 absolute">TNT Man Inc</span>
+			</span>
+
+			<h2 class="text-5xl mt-0 flex justify-start gap-[1ch] leading-0">
+				<p
+					class="student"
+					on:mouseenter={() => animateWord('student')}
+					on:mouseleave={() => endAnimateWord('student')}
+				>
+					{#each 'Student'.split('') as letter}
+						<span class="letter inline-block">{letter}</span>
+					{/each}
+				</p>
+
+				<p
+					class="programmer"
+					on:mouseenter={() => animateWord('programmer')}
+					on:mouseleave={() => endAnimateWord('programmer')}
+				>
+					{#each 'Programmer'.split('') as letter}
+						<span class="letter inline-block">{letter}</span>
+					{/each}
+				</p>
+
+				<p
+					class="gamer"
+					on:mouseenter={() => animateWord('gamer')}
+					on:mouseleave={() => endAnimateWord('gamer')}
+				>
+					{#each 'Gamer'.split('') as letter}
+						<span class="letter inline-block">{letter}</span>
+					{/each}
+				</p>
+			</h2>
+		</h1>
+	</div>
 </section>
-
-<style>
-    /* https://www.youtube.com/watch?v=owpaafxvkjU go brrr */
-
-    .desc:has(.word:hover) .word:not(:hover) {
-        opacity: 0.2;
-    }
-</style>
