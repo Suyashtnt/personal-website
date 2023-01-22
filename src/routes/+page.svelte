504ae77c9a253b91a6ac2220cbf6b9989475da3b
Suyashtnt <suyashtnt@gmail.com>
2023-01-22 21:00:12 +0200

    fix: fix background colour

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,26 @@
 <section class="flex flex-row flex-wrap justify-around content-center">
     <Atropos class="rounded-xl aspect-square pa-8">
         <!-- placeholder image here -->
-        <img src="https://picsum.photos/400/400" alt="placeholder" class="rounded-xl" />
+        <div class="flex content-center h-full w-full justify-center">
+            <img src="https://picsum.photos/400/500" alt="placeholder" class="rounded-xl" />
+        </div>
     </Atropos>
     <div class="flex-grow">
-        <h1 class="text-9xl">
+        <h1 class="text-9xl w-max leading-30">
             Hi,
             <br>
             I'm TNTMan1671
+            <h2 class="text-6xl mt-0 flex justify-between leading-0 desc">
+                <p class="word">Student</p> <p class="word">Programmer</p> <p class="word">Gamer</p>
+            </h2>
         </h1>
     </div>
 </section>
+
+<style>
+    /* https://www.youtube.com/watch?v=owpaafxvkjU go brrr */
+
+    .desc:has(.word:hover) .word:not(:hover) {
+        opacity: 0.2;
+    }
+</style>
