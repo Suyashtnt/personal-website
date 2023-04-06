41590a89414f29ea0f596fd4d70644ea1d830b81
Suyashtnt <suyashtnt@gmail.com>
2023-04-06 23:16:02 +0200

    feat: :lipstick: Change projects grid to be fully dynamic

--- src/lib/bloks/about-sections/Projects.svelte
+++ src/lib/bloks/about-sections/Projects.svelte
@@ -1,25 +1,48 @@
 <script context="module" lang="ts">
-	export interface ProjectsBlok extends SbBlokData {
-		projects: ProjectBlok[]
-	}
+    export interface ProjectsBlok extends SbBlokData {
+        projects: ProjectBlok[]
+    }
 </script>
 
 <script lang="ts">
-	import { storyblokEditable, type SbBlokData } from "@storyblok/svelte"
-	import Project from "./Project.svelte";
-	import type { ProjectBlok } from "./Project.svelte";
+    import { storyblokEditable, type SbBlokData } from "@storyblok/svelte"
+    import Project from "./Project.svelte";
+    import type { ProjectBlok } from "./Project.svelte";
 
-	export let blok: ProjectsBlok;
+    export let blok: ProjectsBlok;
 </script>
 
 <div
-	class="
-		rounded-xl md:pa-2
-		grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4
-		"
+    class="
+        rounded-xl md:pa-2
+        projects
+        "
     use:storyblokEditable={blok}
 >
-	{#each blok.projects as project (project._uid)}
-		<Project blok={project} />
-	{/each}
+    {#each blok.projects as project (project._uid)}
+        <Project blok={project} />
+    {/each}
 </div>
+
+<style>
+/* stolen from https://css-tricks.com/an-auto-filling-css-grid-with-max-columns/ */
+.projects {
+    /**
+    * User input values.
+    */
+    --grid-layout-gap: 1rem;
+    --grid-column-count: 6;
+    --grid-item--min-width: 400px;
+
+    /**
+    * Calculated values.
+    */
+    --gap-count: calc(var(--grid-column-count) - 1);
+    --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
+    --grid-item--max-width: calc((100% - var(--total-gap-width)) / var(--grid-column-count));
+
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr));
+    grid-gap: var(--grid-layout-gap);
+}
+</style>
