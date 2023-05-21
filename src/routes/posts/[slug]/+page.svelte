10e840bb5c50293535b23395e7280f00ff02f79f
Suyashtnt <suyashtnt@gmail.com>
2023-05-21 22:01:09 +0200

    feat: get some better blog post styling

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -5,27 +5,42 @@
 
 	export let data: PageData;
 	import './syntax.css';
+	import './post.css';
 
 	type C = $$Generic<typeof SvelteComponentTyped<unknown, unknown, unknown>>;
 	$: component = data.component as unknown as C;
+
+	const datePublished = new Date(data.frontmatter.date);
+	const dateModified = new Date(data.frontmatter.updated);
+
+	const dateFormatter = new Intl.DateTimeFormat('en-uk', {
+		year: 'numeric',
+		month: 'short',
+		day: '2-digit'
+	});
 </script>
 
 <PageHead title={data.frontmatter.title} description={data.frontmatter.description} />
 
 <svelte:head>
 	<meta property="og:type" content="article" />
-	<meta
-		property="og:article:published_time"
-		content={new Date(data.frontmatter.date).toISOString()}
-	/>
-	<meta
-		property="og:article:modified_time"
-		content={new Date(data.frontmatter.updated).toISOString()}
-	/>
+	<meta property="og:article:published_time" content={datePublished.toISOString()} />
+	<meta property="og:article:modified_time" content={dateModified.toISOString()} />
 </svelte:head>
 
-<div class="flex items-center justify-center">
-	<article class="text-base prose prose-light-text dark:prose-dark-text xl:text-xl w-full">
-		<svelte:component this={component} />
+<div class="flex items-center justify-center mx-2 md:mx-0">
+	<article class="text-lg prose prose-light-text dark:prose-dark-text xl:text-xl w-full">
+		<header class="mb-8 bg-light-mantle dark:bg-dark-mantle rounded-xl pa-4">
+			<h1 class="text-4xl xl:text-5xl my-0">{data.frontmatter.title}</h1>
+			<h2 class="mb-0">By {data.frontmatter.author}.</h2>
+			<p class="mb-0">
+				Published {dateFormatter.format(datePublished)} | Last updated {dateFormatter.format(
+					dateModified
+				)}
+			</p>
+		</header>
+		<p class="text-justify">
+			<svelte:component this={component} />
+		</p>
 	</article>
 </div>
