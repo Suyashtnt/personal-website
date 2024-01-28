0a8d85d589be9fa19db7c809f86c67131e179a0a
Suyashtnt <suyashtnt@gmail.com>
2024-01-28 09:33:52 +0200

    chore: copyright update

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -6,6 +6,6 @@
 <footer
 	class="flex flex-col items-center justify-between rounded-t-2xl bg-light-mantle px-4 text-light-text lg:mx-8 md:flex-row dark:bg-dark-surface_background lg:px-16 dark:text-dark-surface_foreground"
 >
-    <p class="my-4 text-2xl font-semibold">TNTMan1671 | <a href="https://github.com/Suyashtnt/personal-website/blob/master/LICENSE">{m.footer_copyright()} ©️ Suyash 2023</a></p>
+    <p class="my-4 text-2xl font-semibold">TNTMan1671 | <a href="https://github.com/Suyashtnt/personal-website/blob/master/LICENSE">©️ Suyash 2024</a></p>
     <p>{footerText}</p>
 </footer>
